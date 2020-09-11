2a0823e4980e896895927783b5d525b4c2f06ac4
jingmeimei <jingmeimei@kuaishou.com>
2020-09-11 09:25:26 +0800

    旧版的context

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Ref from "./components/ref/Ref";
 import AutoFocusTextInput from './components/ref/AutoFocusTextInput'
+import OldContext from "./components/context/OldContext";
 function App() {
   return (
     <div className="App">
       <Ref/>
       <AutoFocusTextInput/>
+      <OldContext/>
     </div>
   );
 }
