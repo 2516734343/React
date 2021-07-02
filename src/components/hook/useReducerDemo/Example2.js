394413f83d00bf7b920c994b7c1bcb6051637fee
jingmeimei <jingmeimei@kuaishou.com>
2021-07-02 11:46:42 +0800

    初学react

--- src/components/hook/useReducerDemo/Example2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Buttons from './Buttons';
-import Color from './Color';
-import ShowArea from './ShowArea';
-
-export default function Example2() {
-    return <div><Color><ShowArea /><Buttons /></Color></div>
-}
--- src/components/hook/useReducerDemo/ShowArea.js
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from 'react';
-import {ColorContext} from './Color';
-export default function ShowArea() {
-    const { color } = useContext(ColorContext)
-    return <div style={{ color: color }}>颜色为{color}</div>
-}
