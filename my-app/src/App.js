aaa4ca1996e20e0a69f2a22d5a4355290f92c69a
jingmeimei <jingmeimei@kuaishou.com>
2021-07-06 18:15:58 +0800

    react demo 三子棋

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './demo';
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
