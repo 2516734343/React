a7958013e82c850504ff7a9f67005d238822dc4b
jingmeimei <2516734343@qq.com>
2021-08-24 23:09:57 +0800

    保护页面

---
+++ my-app/src/router/Login.js
@@ -0,0 +1,6 @@
+export default function Login() {
+  return <div>
+    <h1>登陆页</h1>
+    <button>登陆</button>
+  </div>
+}
--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 import { render } from 'react-dom'
 
 // 首先我们需要导入一些组件...
-import { Router, Route, Link } from 'react-router'
+// import { Router, Route, Link, withRouter } from 'react-router'
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 function compA() {
   return <div>A组件</div>
@@ -17,3 +18,5 @@ export default function RouterDemo() {
     <Route path='/b' component={compB}></Route>
   </Router>
 }
+
+// export default withRouter(RouterDemo)
