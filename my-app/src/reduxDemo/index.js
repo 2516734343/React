3d0401db1778773ea50e221e9cbd1cf00538214f
2516734343 <2516734343@qq.com>
2021-10-21 09:29:11 +0800

    中间件

--- my-app/src/reduxDemo/index.js
+++ my-app/src/reduxDemo/index.js
@@ -1,4 +1,4 @@
-import { createStore, bindActionCreators } from 'redux';
+import { createStore, bindActionCreators, applyMiddleware } from 'redux';
 import { numberActions } from './action/number-action';
 /**
  * 
@@ -16,11 +16,65 @@ function reducer(state, action) {
   return state;
 }
 
-const store = createStore(reducer, 10);
+const logger1 = store => next => action => {
+  console.log('旧数据', store.getState());
+  console.log('action', action);
+  next(action); // 执行下一个dispatch
+  console.log('新数据', store.getState());
+}
+// 书写一个中间件 在创建仓库后运行
+// 返回一个dispatch
+function loggermiddleWare(store) {
+
+  console.log('中间件1');
+  console.log(store);
+  return function (next) { // next是下一个中间件传递过来的dispatch
+    // 返回的函数就是最终要运用的dispatch函数
+    return function dispath(action) {
+      console.log('旧数据', store.getState());
+      console.log('action', action);
+      next(action); // 执行下一个dispatch
+      console.log('新数据', store.getState());
+
+    }
+  }
+
+}
+function logger2(store) {
+  //
+  console.log('中间件2');
+  console.log(store);
+
+  return function (next) {  // next是下一个中间件传递过来的dispatch
+    // 返回的函数就是最终要运用的dispatch函数
+    return function dispath(action) {
+
+      console.log('旧数据', store.getState());
+      console.log('action', action);
+      next(action); // 执行下一个dispatch
+      console.log('新数据', store.getState());
+    }
+  }
+
+
+}
+
+// 应用中间件方式1
+const store = createStore(reducer, 10, applyMiddleware(loggermiddleWare, logger2));
+
+// 方式2
+// - applyMiddleware 函数，用于记录有哪些中间件，它会返回一个函数
+//   - 该函数用于记录创建仓库的方法，然后又返回一个函数
+// applyMiddleware(logger1, logger2)(createStore)(reducer, 10);
+
+
+
 
 const action = {
   type: 'increase'
 }
+
+
 //第一个参数，是action创建函数合并的对象，第二个参数是仓库的dispatch函数
 //得到一个新的对象，新对象中的属性名与第一个参数的属性名一致
 // const boundActions = bindActionCreators(numberActions, store.dispatch);
@@ -57,5 +111,13 @@ console.log(store.getState());
 // }));
 
 // console.log(store.getState());
+
+
+
+
+
+
+
+
 
 export default store;
