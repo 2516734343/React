4bc8f6e07293d6bc22e3d1db097d1bae123a632e
2516734343 <2516734343@qq.com>
2022-03-14 22:28:37 +0800

    useEffect

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,25 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+const ref = React.createRef();
+let timer = null;
+let curTimes = 0;
+function MoveBlock(props) {
+  useEffect(() => {
+    clearInterval(timer);
+    const div = ref.current;
+    const disX = props.left / 100;
+    const disY = props.top / 100;
+    timer = setInterval(() => {
+      curTimes++;
+      const newLeft = disX * curTimes;
+      const newTop = disY * curTimes;
+      div.style.left = newLeft + "px";
+      div.style.top = newTop + "px";
+      if (curTimes === 100) {
+        clearInterval(timer);
+      }
+    }, 10);
+    return () => {
+      console.log('清理函数')
+    }
+  }, [props.left, props.top]);
+
+  return <div style={{
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    left: props.left,
+    top: props.top,
+  }} ref={ref}></div>
+}
 function App() {
-  const [count, setCount] = useState(1);
+  const [point, setPoint] = useState({ x: 0, y: 0 });
   const [visible, setVisible] = useState(true);
+
   return (
     <div className="App">
-      {
-        visible &&
-        <div>
-          <button onClick={() => setCount(count - 1)}>-</button>
-          <span>{count}</span>
-          <button onClick={() => {
-            setCount(count => count + 1)
-            setCount(count => count + 1)
-          }}>+</button>
-        </div>
-      }
-      <button onClick={() => setVisible(!visible)}>{visible ? '隐藏' : '显示'}</button>
+      x: <input value={point.x} onChange={e => {
+        setPoint({
+          ...point,
+          x: parseInt(e.target.value)
+        })
+      }} />
+      y: <input value={point.y} onChange={e => {
+        setPoint({
+          ...point,
+          y: parseInt(e.target.value)
+        })
+      }} />
+
+
+      <MoveBlock left={point.x} top={point.y} />
     </div>
   );
 }
